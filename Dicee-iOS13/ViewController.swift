68cfbacadecbcf08ede4081d11845c1315caca8a
vivektarun <vivektarun1234@gmail.com>
2023-03-22 18:35:09 +0530

    App completed

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,26 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     // IB outlet allow me to reference a UI element.
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageview2: UIImageView!
     
+//    var leftDiceNumber = 1;
+//    var rightDiceNumber = 5;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
         
-        
-        diceImageview2.image = UIImage(imageLiteralResourceName: "DiceSix")
         
     }
-
+    
     @IBAction func rollButtonPressed(_ sender: UIButton ) {
-//        print("button got tapped. ")
-        diceImageview2.image = UIImage(imageLiteralResourceName: "DiceFour")
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceFour")
+        //        print("button got tapped. ")
+        
+        
+        diceImageView1.image = [UIImage(imageLiteralResourceName: "DiceOne"),        UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix")][Int.random(in: 1...5)];
+//        leftDiceNumber = leftDiceNumber + 1;
+        
+        diceImageview2.image = [UIImage(imageLiteralResourceName: "DiceOne"),        UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix")][Int.random(in: 1...5)];
+//        rightDiceNumber = rightDiceNumber - 1;
+        
+//        print(Int.random(in: 1...10))
     }
     
 }
-
